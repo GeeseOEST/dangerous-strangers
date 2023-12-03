486d0671aa11758f6266e5d68c8dc1c13b4417bc
GeeseOEST <122099226+GeeseOEST@users.noreply.github.com>
2023-12-03 14:28:18 +0100

    Create a package folder and the intial main file

---
+++ dangerousstrangers/dangerousstrangers.py
@@ -0,0 +1,12 @@
+'''
+ Takes in the user info, randomizes the other components, and then sends it to the CharacterBuilder class. Once finished, this recieves that back and then prints it out. 
+ This somewhat makes this file complex as we're not taking the randomization or printing/exporting logic out.I would like to do in order to be able to have some type of adaptor/dependency injection/whatever to be able to substitute different ways of export (pdf, print, view in web page, ...) but for now is a requirement of the CS50 approach to submitting a final project.
+ '''
+ 
+def main():
+    ... 
+     
+    
+
+if __name__ == '__main__':
+    main()
