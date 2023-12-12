48c4d36c738abe407785ebd0cc19df1c8d5eced8
GeeseOEST <122099226+GeeseOEST@users.noreply.github.com>
2023-12-12 15:40:03 +0100

    Creates json_loader file and load_top_level_keys function

--- dangerousstrangers/dangerousstrangers.py
+++ dangerousstrangers/dangerousstrangers.py
@@ -1,22 +1,10 @@
 import random
+import json
 
 """
 Takes in the user info, randomizes the other components, and then sends it to the CharacterBuilder class. Once finished, this recieves that back and then prints it out. 
 This somewhat makes this file complex as we're not taking the randomization or printing/exporting logic out.I would like to do in order to be able to have some type of adaptor/dependency injection/whatever to be able to substitute different ways of export (pdf, print, view in web page, ...) but for now is a requirement of the CS50 approach to submitting a final project.
 """
-
-
-"""
-TODO - All of the below
-Functions required here;
-    **HOLD - Not the simplest, so to begin with will not implement this.** Take input from user - Somewhat Outlined
-    Randomize any aspectsuser hasn't provided via args - Outlined
-    DONE Roll ability scores - Outlined
-    Hand all off to CharacterBuilder class
-        CharacterBuilder class will require Race class, Class class, Background class - to behave as a way of bundling the object itself for use in Python (Maybe?)
-    Print the character - Outlined
-"""
-
 
 def main():
     roll_scores()
---
+++ json_loader.py
@@ -0,0 +1,40 @@
+import json
+
+
+def load_top_level_keys(component: str) -> list:
+    """Loads in a JSON file, extracts the top-level keys, and then closes the JSON file again.
+
+    Args:
+        component (str): The type of the component that should be loaded EG Race
+
+    Returns:
+        list: List of all keys in the JSON file at the top level
+    """
+    file_name = f"{component}s.json"
+
+    try:
+        with open(file_name, "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        raise FileNotFoundError(
+            f"File {file_name} not found in same folder as json_loader"
+        )
+    except json.JSONDecodeError:
+        raise ValueError("Invalid JSON file")
+
+    keys = [key for key in data]
+
+    return keys
+
+def load_chosen_component(component_type: str, component: str) -> dict:
+    """Loads the randomly selected component into memory for use when creating classes
+
+    Args:
+        component_type(str): The type of component that is being addressed EG race
+        component (str): The selected component from the above type EG human
+
+    Returns:
+        dict: Full dictionary object of everything contained in the JSON description that can be used to create a class of that object
+    """
+    ...
+    
