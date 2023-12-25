c72b52b6a7f6007de502c1c19f120d9e1e7fc97d
GeeseOEST <122099226+GeeseOEST@users.noreply.github.com>
2023-12-25 17:22:06 +0100

    Create basic initialization to empty defaults

--- dangerousstrangers/race.py
+++ dangerousstrangers/race.py
@@ -7,22 +7,47 @@ import json_loader
 class Race:
     def __init__(self, race, test_type=None) -> None:
         
+        ability_scores = ["STR", "DEX", "CON", "INT", "WIS", "CHA"]
+        proficiency_types = ["armor", "weapon", "tool", "save", "skill"]
+        
         if race == "test":
             rules = json_loader.load_test_file("race", test_type)
+            self.rules = rules # Allows the test files to see what rules the class was based on and compare to final outputs
         else:
             rules = json_loader.load_chosen_component("race", race)
+        
+        # Sets everything to intialized so all Race classes have the same attributes for adding together later
+        
+        self.race = race
+        self.age = 0
+        self.size = rules["size"]
+        self.speed = rules["speed"]
+        self.ability_score_modifiers = {}
+        self.languages = []
+        self.language_choices = 0
+        self.proficiencies = {}
+        self.attributes = {}
+        
+        for score in ability_scores:
+            self.ability_score_modifiers[score] = 0
+        
+        for proficiency in proficiency_types:
+            self.proficiencies[proficiency] = []
+                  
+        
+    def set_ability_scores(self, rules):    # Updates ability scores
+        ...
     
-
     def set_age(self, rules):   # Randomises age, return value and maturity level (potentially?)
         ...
         
     def set_languages(self, rules):     # Sets the standard languages and counts the selectable number of languages for later picking in CharacterBuilder class
         ...
         
-    def set_proficiencies():    # Sets the standard proficiencies
+    def set_proficiencies(self, rules):    # Sets the standard proficiencies
         ...
         
-    def set_attributes():   # Sets the standard attributes
+    def set_attributes(self, rules):   # Sets the standard attributes
         ...
         
 
