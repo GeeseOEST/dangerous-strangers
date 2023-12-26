fe74f068c0338ebbc48fbc01892a523749675e89
GeeseOEST <122099226+GeeseOEST@users.noreply.github.com>
2023-12-26 17:47:20 +0100

    Adds default self.attribute setting

--- dangerousstrangers/background.py
+++ dangerousstrangers/background.py
@@ -1,10 +1,45 @@
+import dangerousstrangers.json_loader
+
 #Class definition for the Background class that will become a component of the Character class, created via the CharacterBuilder class
 
 # Has many set_COMPONENT methods as not all fields in JSON exist and therefore the creation requires setting them to empty before then updating to rules so that each class contains all of the expected components
 
 class Background:
     def __init__(self, background, test_type=None) -> None:
-        pass
+        
+        if background == "test":
+            rules = dangerousstrangers.json_loader.load_test_file("background", test_type)
+            self.rules = rules # Allows the test files to see what rules the class was based on and compare to final outputs
+        else:
+            rules = dangerousstrangers.json_loader.load_chosen_component("background", background)
+            
+        self.background = background
+        self.proficiencies = {
+            "armor": [],
+            "weapon": [],
+            "tool": [],
+            "save": [],
+            "skill": [],
+        }
+        self.selectable_proficiencies = {
+            "armor": [0, []],
+            "weapon": [0, []],
+            "tool": [0, []],
+            "save": [0, []],
+            "skill": [0, []],
+        }
+        self.languages = []
+        self.language_choices = 0
+        self.equipment = []
+        self.money ={
+            "CP": 0,
+            "SP": 0,
+            "EP": 0,
+            "GP": 0,
+            "PP": 0
+        }
+        self.features = {}      
+
     
     def set_proficiencies(self, rules):    # Set the standard proficiences and + the number of selectable proficiencies for selection in the CharacterBuilder class
         ...
